87043e012dc59b48c33488c0a717f5a151fb9996
cdc-dpbrown <ita3@cdc.gov>
2020-08-05 07:39:59 -0400

    Added HtmlAgilityPack and removed unsupported states.

--- src/Cdc.Covid.WebScraper/MetadataReport.cs
+++ src/Cdc.Covid.WebScraper/MetadataReport.cs
@@ -21,22 +21,23 @@ namespace Cdc.Covid.WebScraper
             SourceTypeText.Add(SourceTypes.XPATH, "Excel (.xlsx)");
             SourceTypeText.Add(SourceTypes.Zip, "Zip File (.zip)");
 
-            StateInfos.Add(new StateScrapeInfo("Alabama", "AL", SourceTypes.Custom, "https://dph1.adph.state.al.us/covid-19/", ""));
-            StateInfos.Add(new StateScrapeInfo("Florida", "FL", SourceTypes.ArcGIS, "https://experience.arcgis.com/experience/96dd742462124fa0b38ddedb9b25e429", ""));
+            //StateInfos.Add(new StateScrapeInfo("Alabama", "AL", SourceTypes.Custom, "https://dph1.adph.state.al.us/covid-19/", ""));
+            //StateInfos.Add(new StateScrapeInfo("Florida", "FL", SourceTypes.ArcGIS, "https://experience.arcgis.com/experience/96dd742462124fa0b38ddedb9b25e429", ""));
 
             Expression expression = new Expression();
             expression.FileName = "countycases.csv";
-
             StateInfos.Add(new StateScrapeInfo("Georgia", "GA", SourceTypes.Zip, "https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip", JsonSerializer.Serialize(expression)));
-            StateInfos.Add(new StateScrapeInfo("Kentucky", "KY", SourceTypes.Custom, "https://kygeonet.maps.arcgis.com/apps/opsdashboard/index.html#/543ac64bc40445918cf8bc34dc40e334", ""));
+            
+            //StateInfos.Add(new StateScrapeInfo("Kentucky", "KY", SourceTypes.Custom, "https://kygeonet.maps.arcgis.com/apps/opsdashboard/index.html#/543ac64bc40445918cf8bc34dc40e334", ""));
 
             expression = new Expression();
             expression.XPath = "/html/body/div[1]/div[3]/div[5]/div/table[1]/tbody";
-
             StateInfos.Add(new StateScrapeInfo("Mississippi", "MS", SourceTypes.XPATH, "https://msdh.ms.gov/msdhsite/_static/14,0,420.html#Mississippi", JsonSerializer.Serialize(expression)));
-            StateInfos.Add(new StateScrapeInfo("North Carolina", "NC", SourceTypes.HTML, "https://covid19.ncdhhs.gov/dashboard/about-data", ""));
-            StateInfos.Add(new StateScrapeInfo("South Carolina", "SC", SourceTypes.HTML, "https://www.scdhec.gov/infectious-diseases/viruses/coronavirus-disease-2019-covid-19/sc-cases-county-zip-code-covid-19", ""));
-            StateInfos.Add(new StateScrapeInfo("Tennesee", "TN", SourceTypes.ArcGIS, "https://experience.arcgis.com/experience/885e479b688b4750837ba1d291b85aed", ""));
+            
+            //StateInfos.Add(new StateScrapeInfo("North Carolina", "NC", SourceTypes.HTML, "https://covid19.ncdhhs.gov/dashboard/about-data", ""));
+            //StateInfos.Add(new StateScrapeInfo("South Carolina", "SC", SourceTypes.HTML, "https://www.scdhec.gov/infectious-diseases/viruses/coronavirus-disease-2019-covid-19/sc-cases-county-zip-code-covid-19", ""));
+            //StateInfos.Add(new StateScrapeInfo("Tennesee", "TN", SourceTypes.ArcGIS, "https://experience.arcgis.com/experience/885e479b688b4750837ba1d291b85aed", ""));
+            
             StateInfos.Add(new StateScrapeInfo("Michigan", "MI", SourceTypes.Excel, "https://www.michigan.gov/documents/coronavirus/Cases_and_Deaths_by_County_2020-07-24_697248_7.xlsx", ""));
         }
     }
---
+++ src/Cdc.Covid.WebScraper/XPath_Scraper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cdc.Covid.WebScraper
+{
+    public class XPath_Scraper : IStateScraper
+    {
+        private string _state = string.Empty;
+        private string _stateAbbreviation = string.Empty;
+        private SourceTypes _sourceType;
+        private string _source = string.Empty;
+        private Expression _expression = new Expression();
+
+
+        public XPath_Scraper(StateScrapeInfo info)
+        {
+            _state = info.State;
+            _stateAbbreviation = info.StateAbbreviation;
+            _sourceType = info.SourceType;
+            _source = info.Source;
+            _expression = info.ExpressionObject;
+        }
+
+        public async Task<StateReport> ExecuteScrapeAsync()
+        {
+            List<CountyReport> countyReports = new List<CountyReport>(250);
+
+            HttpClient client = new HttpClient();
+            using (var file = await client.GetStreamAsync(_source).ConfigureAwait(false))
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+
+                using (ZipArchive z = new ZipArchive(memoryStream))
+                {
+                    ZipArchiveEntry zEntry = z.Entries.FirstOrDefault(e => e.Name.Contains(_expression.FileName));
+
+                    using (Stream csvStream = zEntry.Open())
+                    using (StreamReader reader = new StreamReader(csvStream))
+                    {
+                        bool headersLine = true;
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            var cells = line.Split(',');
+
+                            if (headersLine)
+                            {
+                                headersLine = false;
+                            }
+                            else
+                            {
+                                string name = cells[0];
+                                int confirmed = int.Parse(cells[1]);
+                                int deaths = int.Parse(cells[2]);
+                                int hospitalizations = int.Parse(cells[3]);
+                                double rate = double.Parse(cells[4]);
+
+                                CountyReport report = new CountyReport(
+                                    name: name, 
+                                    confirmed: confirmed, 
+                                    probable: -1, 
+                                    deaths: deaths, 
+                                    hospitalizations: hospitalizations,
+                                    rate: rate);
+
+                                countyReports.Add(report);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return new StateReport(state: _state, abbreviation: _stateAbbreviation, reports: countyReports);
+        }
+    }
+}
